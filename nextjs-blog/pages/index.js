4431747e9a73761090782b18e961858d3f2eee69
Rharuow <haryssonsoares@gmail.com>
2020-07-14 11:26:34 -0300

    refactor code

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>My name is Harysson and I'm 29 years old. I'm biologist and web developer. To contact-me by instagram <a href="https://www.instagram.com/harysson_soares/">Harysson Soares</a> </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
